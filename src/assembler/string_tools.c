1e9bc600a3a6bcd526a6ed6e18904338a718b54b
Potato Chronicler <potater@potater.dev>
2025-09-04 14:02:43 +0200

    chore: remove unused duplicate function

--- src/assembler/string_tools.c
+++ src/assembler/string_tools.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-
-// Copies a string into a new dynamically allocated buffer.
-char* read_string(const char *str) {
-    if (!str) return NULL;
-    size_t len = strlen(str);
-    char *buffer = malloc(len + 1);
-    if (!buffer) return NULL;
-    strcpy(buffer, str);
-    return buffer;
-}
 
 long get_file_size(const char *path) {
     FILE *file = fopen(path, "rb");  // open in binary mode
