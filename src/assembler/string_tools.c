e748ea902ddd1932fac224717af930dfc2a5ea1e
Potato Chronicler <potater@potater.dev>
2025-09-04 14:45:42 +0200

    style: remove useless comments

--- src/assembler/string_tools.c
+++ src/assembler/string_tools.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 long get_file_size(const char *path) {
-    FILE *file = fopen(path, "rb");  // open in binary mode
+    FILE *file = fopen(path, "rb");
     if (!file) return -1;            // error opening file
 
-    fseek(file, 0, SEEK_END);        // move to the end of the file
+    fseek(file, 0, SEEK_END);
     long size = ftell(file);         // get current position = file size in bytes
     fclose(file);
 
