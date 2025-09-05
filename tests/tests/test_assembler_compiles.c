9dcf42be1a4e3d769ec9ba45194507c8937c842d
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-09-05 08:55:13 +0200

    feat: add Unity

---
+++ tests/tests/test_assembler_compiles.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+void setUp(void) {}  
+void tearDown(void) {}
+
+void compile(void) {
+   // TODO int return_val = init("../testdata/test.asm");
+   // TEST_ASSERT_EQUAL(0, return_val);
+   
+}
+
+int main(void) {
+   UNITY_BEGIN();
+   RUN_TEST(compile);
+   return UNITY_END();
+}
