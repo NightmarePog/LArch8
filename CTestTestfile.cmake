6cb32c2fcc1ac57f100005ba790f716aebd6bfbf
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-09-10 17:35:12 +0200

    feat: compialation stuff

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nightmare/Documents/GitHub/CPU-project
+# Build directory: /home/nightmare/Documents/GitHub/CPU-project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(assembler_test "/home/nightmare/Documents/GitHub/CPU-project/run_tests")
+set_tests_properties(assembler_test PROPERTIES  _BACKTRACE_TRIPLES "/home/nightmare/Documents/GitHub/CPU-project/CMakeLists.txt;76;add_test;/home/nightmare/Documents/GitHub/CPU-project/CMakeLists.txt;0;")
